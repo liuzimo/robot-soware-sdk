7c8f39edcc53ea219a05b429f284e5a2e3c96040
liuzimo <919825501@qq.com>
2019-09-26 09:45:24 +0800

    需使用自己的key

--- Native.Csharp/App/LuaEnv/BaiDuApi.cs
+++ Native.Csharp/App/LuaEnv/BaiDuApi.cs
@@ -12,12 +12,12 @@ namespace Native.Csharp.App.LuaEnv
     class BaiDuApi
     {
         //本地图片文字识别
-        public static string GeneralBasic(string filename)
+        public static string GeneralBasic(string filename,string API_KEY,string SECRET_KEY)
         {
             try { 
                 //var APP_ID = "你的 App ID";
-                var API_KEY = "M5uerfD1rEdXT6ddwCYK2tYs";
-                var SECRET_KEY = "ZTG6GYmrmXU655njhs6RVAMGFGY7Is8E";
+                //var API_KEY = "M5uerfD1rEdXT6ddwCYK2tYs";
+                //var SECRET_KEY = "ZTG6GYmrmXU655njhs6RVAMGFGY7Is8E";
                 var client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY);
 
 
@@ -49,12 +49,9 @@ namespace Native.Csharp.App.LuaEnv
         }
 
         //链接图片识别文字
-        public static void GeneralBasicUrl(string url)
+        public static void GeneralBasicUrl(string url, string API_KEY, string SECRET_KEY)
         {
             try { 
-                //var APP_ID = "你的 App ID";
-                var API_KEY = "M5uerfD1rEdXT6ddwCYK2tYs";
-                var SECRET_KEY = "ZTG6GYmrmXU655njhs6RVAMGFGY7Is8E";
                 var client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY);
 
                 // 调用通用文字识别, 图片参数为远程url图片，可能会抛出网络等异常，请使用try/catch捕获
@@ -79,11 +76,11 @@ namespace Native.Csharp.App.LuaEnv
 
         
         // 通用物体和场景识别
-        public static string AdvancedGeneral(string filename)
+        public static string AdvancedGeneral(string filename,string token)
         {
             try
             {
-                string token = "24.5e680d741ae5c7ce3528d02aa3923528.2592000.1571734533.282335-17306771";
+                //string token = "24.5e680d741ae5c7ce3528d02aa3923528.2592000.1571734533.282335-17306771";
                 string host = "https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general?access_token=" + token;
                 Encoding encoding = Encoding.Default;
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
